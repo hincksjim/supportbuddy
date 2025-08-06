7c85f7e39aef6afe2348740f6bdb8455563c0896
james hincks <hincksj@gmail.com>
2025-08-06 08:51:34 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,14 +4,14 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-   <div className="flex h-screen flex-col">
-      <Header />
-      <div className="flex flex-1 overflow-hidden">
-        <SidebarProvider>
-            <AppShell />
-            <main className="flex-1 overflow-y-auto">{children}</main>
-        </SidebarProvider>
+    <SidebarProvider>
+      <div className="flex h-screen flex-col">
+        <Header />
+        <div className="flex flex-1 overflow-hidden">
+          <AppShell />
+          <main className="flex-1 overflow-y-auto">{children}</main>
+        </div>
       </div>
-    </div>
+    </SidebarProvider>
   )
 }

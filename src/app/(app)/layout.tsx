ec68f019af0e9ca50edb9e94a786cc70c2c67dee
james hincks <hincksj@gmail.com>
2025-08-06 08:51:10 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <AppShell>
    
    with t

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,12 +4,14 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
-        <AppShell />
-        <div className="flex-1 flex flex-col h-screen overflow-hidden">
-            <Header />
+   <div className="flex h-screen flex-col">
+      <Header />
+      <div className="flex flex-1 overflow-hidden">
+        <SidebarProvider>
+            <AppShell />
             <main className="flex-1 overflow-y-auto">{children}</main>
-        </div>
-    </SidebarProvider>
+        </SidebarProvider>
+      </div>
+    </div>
   )
 }

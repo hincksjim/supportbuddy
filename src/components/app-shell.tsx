124e7558f4a1fcaab054f324604b616a1495f6d8
james hincks <hincksj@gmail.com>
2025-08-06 08:56:26 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -44,16 +44,16 @@ export function AppShell() {
             <SidebarMenu>
                 {navItems.map((item) => (
                     <SidebarMenuItem key={item.href}>
-                        <Link href={item.href} passHref>
-                             <SidebarMenuButton
-                                asChild
-                                isActive={pathname.startsWith(item.href)}
-                                tooltip={item.label}
-                             >
+                         <SidebarMenuButton
+                            asChild
+                            isActive={pathname.startsWith(item.href)}
+                            tooltip={item.label}
+                         >
+                            <Link href={item.href}>
                                 <item.icon />
                                 <span>{item.label}</span>
-                             </SidebarMenuButton>
-                        </Link>
+                            </Link>
+                         </SidebarMenuButton>
                     </SidebarMenuItem>
                 ))}
             </SidebarMenu>

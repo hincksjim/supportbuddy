9166b226183a1a2625deead1d3ac614fe08f76e1
james hincks <hincksj@gmail.com>
2025-09-26 11:16:09 +0000

    `projects/generativelanguage-ga/locations/us-central1/publishers/google/

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({location: 'europe-west1'})],
   model: googleAI.model('gemini-1.5-flash'),
 });
